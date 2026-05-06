abdceaebc53dfcf3878912853bfa38d4ad86ac48
lijun9190 <1677693040@qq.com>
2026-05-06 22:51:07 +0800

    refactor: 优化全局异常处理
    - 增加请求上下文日志（URI、Method、IP）
    - 区分 JWT 过期和签名异常
    - 调整日志级别（业务异常用 info，系统异常用 error）
    - 增加 HTTP 异常处理（405、404、415）
    - 将 getErrors 方法改为 private
    - 增强 MyCustomException 日志信息

--- hire-common/src/main/java/com/imooc/exceptions/GraceExceptionHandler.java
+++ hire-common/src/main/java/com/imooc/exceptions/GraceExceptionHandler.java
@@ -7,16 +7,21 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.zookeeper.KeeperException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.SignatureException;
 import java.util.HashMap;
 import java.util.List;
@@ -28,6 +33,9 @@ import javax.validation.ConstraintViolationException;
 @RestControllerAdvice
 public class GraceExceptionHandler {
 
+    @Autowired
+    private HttpServletRequest request;
+
 //    @ExceptionHandler(ArithmeticException.class)
 //    public GraceJSONResult returnArithmeticException(ArithmeticException e) {
 //        log.error("Arithmetic exception", e);
@@ -36,19 +44,28 @@ public class GraceExceptionHandler {
 
     @ExceptionHandler(KeeperException.BadVersionException.class)
     public GraceJSONResult returnBadVersionException(KeeperException.BadVersionException e) {
-        log.warn("Zookeeper bad version", e);
+        log.info("Zookeeper bad version - URI: {}, Method: {}, IP: {}",
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr());
         return GraceJSONResult.exception(ResponseStatusEnum.ZOOKEEPER_BAD_VERSION_ERROR);
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public GraceJSONResult returnMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
-        log.warn("Uploaded file exceeds max size", e);
+        log.warn("Uploaded file exceeds max size - URI: {}, Method: {}, IP: {}",
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr());
         return GraceJSONResult.exception(ResponseStatusEnum.FILE_MAX_SIZE_500KB_ERROR);
     }
 
     @ExceptionHandler(MyCustomException.class)
     public GraceJSONResult returnMyCustomException(MyCustomException e) {
-        log.warn("Business exception: status={}, msg={}",
+        log.info("Business exception - URI: {}, Method: {}, IP: {}, status={}, msg={}",
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr(),
                 e.getResponseStatusEnum().status(),
                 e.getResponseStatusEnum().msg());
         return GraceJSONResult.exception(e.getResponseStatusEnum());
@@ -56,7 +73,10 @@ public class GraceExceptionHandler {
 
     @ExceptionHandler(ExpiredJwtException.class)
     public GraceJSONResult returnExpiredJwtException(ExpiredJwtException e) {
-        log.warn("JWT expired", e);
+        log.info("JWT expired - URI: {}, Method: {}, IP: {}",
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr());
         return GraceJSONResult.exception(ResponseStatusEnum.JWT_EXPIRE_ERROR);
     }
 
@@ -67,7 +87,10 @@ public class GraceExceptionHandler {
             io.jsonwebtoken.security.SignatureException.class
     })
     public GraceJSONResult returnSignatureException(Exception e) {
-        log.warn("JWT signature or format invalid", e);
+        log.warn("JWT signature or format invalid - URI: {}, Method: {}, IP: {}",
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr());
         return GraceJSONResult.exception(ResponseStatusEnum.JWT_SIGNATURE_ERROR);
     }
 
@@ -97,17 +120,52 @@ public class GraceExceptionHandler {
             MissingServletRequestParameterException.class
     })
     public GraceJSONResult returnRequestParamException(Exception e) {
-        log.warn("Request parameter error", e);
+        log.warn("Request parameter error - URI: {}, Method: {}, IP: {}",
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr());
+        return GraceJSONResult.exception(ResponseStatusEnum.SYSTEM_PARAMS_SETTINGS_ERROR);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public GraceJSONResult returnMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        log.warn("HTTP method not supported - URI: {}, Method: {}, IP: {}",
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr());
+        return GraceJSONResult.exception(ResponseStatusEnum.SYSTEM_PARAMS_SETTINGS_ERROR);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public GraceJSONResult returnNoHandlerFoundException(NoHandlerFoundException e) {
+        log.warn("No handler found - URI: {}, Method: {}, IP: {}",
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr());
+        return GraceJSONResult.exception(ResponseStatusEnum.SYSTEM_ERROR);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public GraceJSONResult returnMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        log.warn("Media type not supported - URI: {}, Method: {}, IP: {}, Content-Type: {}",
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr(),
+                request.getContentType());
         return GraceJSONResult.exception(ResponseStatusEnum.SYSTEM_PARAMS_SETTINGS_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
     public GraceJSONResult returnException(Exception e) {
-        log.error("Unexpected system exception", e);
+        log.error("Unexpected system exception - URI: {}, Method: {}, IP: {}",
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr(),
+                e);
         return GraceJSONResult.exception(ResponseStatusEnum.SYSTEM_ERROR);
     }
 
-    public Map<String, String> getErrors(BindingResult result) {
+    private Map<String, String> getErrors(BindingResult result) {
 
         Map<String, String> map = new HashMap<>();
 
